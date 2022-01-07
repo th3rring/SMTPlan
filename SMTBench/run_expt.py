39bc2998c020f3cf138a7b35f687478a9e103e83
Thomas Herring <teh6@rice.edu>
2022-01-06 18:10:33 -0600

    Added script to run basic experiments

---
+++ SMTBench/run_expt.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import subprocess 
+import shlex
+import os.path
+import sys
+
+proc_path = "../SMTPlan/build/SMTPlan"
+
+# Print error and exit if executable isn't found
+if not os.path.isfile(proc_path):
+    sys.stderr.write("Executable %s was not found\n" % proc_path)
+    sys.exit(1)
+
+print("SMTPlan benchmark")
+
+
+domain = "../benchmarks/generator_nonlinear/gen_nonlinear_domain.pddl"
+problem = "../benchmarks/generator_nonlinear/gen_nonlinear_prob07.pddl"
+
+proc = subprocess.Popen([proc_path, domain, problem], stdout=subprocess.PIPE, universal_newlines=True)
+
+outs = proc.stdout.read()
+
+
+# Prints the standard output from the process!
+# print(shlex.split(outs.decode("utf-8")))
+print(outs)
+# print(shlex.split(outs))
+
+sys.exit(0)
+
