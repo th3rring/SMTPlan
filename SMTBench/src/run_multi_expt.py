14926c2d14b7b3de8bf7b30a9fa06f0e1bba41ed
Thomas Herring <therring@pop-rice.localdomain>
2022-01-21 14:18:00 -0600

    Added script to run an expt many times

--- SMTBench/src/run_multi_expt.py
+++ SMTBench/src/run_multi_expt.py
@@ -13,6 +13,9 @@ def data_generator(data):
 # Set an upper bound on how many iterations we're allowing
 upper_bound = 20
 
+# Number of experiments to run
+num_expts = 5
+
 # Set the domain
 domain = "conveyor_domain_transfer.pddl"
 
@@ -32,7 +35,7 @@ print("--- SMTPlan Benchmark ---")
 print("Current time {}-{}".format(date, time))
 print("Domain: {}".format(domain))
 print("Problem: {}".format(problem))
-print("Single experiment run...")
+print("Number of Experiments: {}".format(num_expts))
 
 # Create expt database, currently using unique datetime
 con = sqlite3.connect(experiments_dir + "expt_{}.db".format(datetime.now().strftime("%Y_%m_%d-%H:%M:%S")))
@@ -49,19 +52,20 @@ problem = problem_dir + problem
 
 # Create planner process
 proc = PlannerProcess(date, time)
+results = []
 
 # Run the experiment
-results = proc.run_expt(domain, problem, upper_bound, verbose)
+results.append(proc.run_expt(domain, problem, upper_bound, verbose))
 
 # Insert into the database
-cur.executemany("insert into experiments values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data_generator([results]))
+cur.executemany("insert into experiments values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data_generator(results))
 
 print("Experiment date: {}".format(date))
 print("Experiment time: {}".format(time))
 
 # Printout the log from this experiment if we're in verbose
 if verbose:
-    print(results[-1])
+    print(results[-1][-1])
 
 # Save
 con.commit()
